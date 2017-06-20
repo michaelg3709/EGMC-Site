1cb41b98f2488f81c96fe142e1feae1b873add3d
michaelg3709 <michaelgeorge032@gmail.com>
2017-06-20 15:43:53 +0100

    Update responsive-menu.js

--- js/responsive-menu.js
+++ js/responsive-menu.js
@@ -608,7 +608,7 @@
             e.stopPropagation();
 
             if ( ( $el.hasClass( o.itemHoverClass ) || !$el.hasClass( o.parentClass ) ) && !o.touchMoveBool ) {
-                location.href = $el.children( 'a' ).attr('href=#' target="_blank");
+                location.href = $el.children( 'a' ).attr('href');
                 menuBlur( e );
             } else if ( e.type !== 'touchend' ) {
                 e.preventDefault();
